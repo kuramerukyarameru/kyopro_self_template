091f838624f821060f6c026d28d5431486e6f971
kuramerukyarameru <nagisa.miyazaki1116@gmail.com>
2026-08-31 00:35:57 +0900

    Initialize standard.cpp with common headers and macros
    Added standard includes and utility macros for competitive programming.

--- template/standard.cpp
+++ template/standard.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <numeric>
+#include <functional>
+#include <tuple>
+#include <limits>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <array>
+#include <cstdint>
+#include <cassert>
+using namespace std;
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define REP(i,l,r) for(int i=l;i<(r);++i)
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define el "\n"
+using ll = long long;using vi = vector<int>;using vll = vector<ll>;using vb = vector<bool>;const ll INF = 1LL<<60;
+template<class T> using pq=priority_queue<T>;template<class T> using pq_gt=priority_queue<T,vector<T>,greater<T>>;
+template<class T> inline bool chmin(T&a,T b){if(a>b){a=b;return true;}return false;};
+template<class T> inline bool chmax(T&a,T b){if(a<b){a=b;return true;}return false;};
+const int dr4[4]={-1,0,1,0};const int dc4[4]={0,1,0,-1};
+const int dr8[8]={-1,-1,-1,0,1,1,1,0};const int dc8[8]={-1,0,1,1,1,0,-1,-1};
+//上右下左//左上上右上右右下下左下左
 
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+    
+
+    return 0;
+}
