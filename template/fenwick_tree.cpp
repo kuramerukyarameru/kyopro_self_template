036c4c1eef888e1275c697f04d187b337ee3e9f8
kuramerukyarameru <nagisa.miyazaki1116@gmail.com>
2026-09-01 14:43:12 +0900

    Add Fenwick Tree implementation in C++
    Implemented Fenwick Tree (Binary Indexed Tree) for efficient range queries and updates.

--- template/fenwick_tree.cpp
+++ template/fenwick_tree.cpp
@@ -1,1 +1,12 @@
-
+template<class T>
+struct fenwick_tree{
+    int n;
+    vector<T> vec;
+    fenwick_tree(int size):n(size){vec.resize(size+1,T(0));}
+    void add(int p,T x){for(int i=p+1;i<=n;i+=i&-i){vec[i]+=x;}}
+    T sum(int l,int r){T resright=(T)0;T resleft=(T)0;
+        for(int i=r;i>0;i-=i&-i){resright+=vec[i];}
+        for(int i=l;i>0;i-=i&-i){resleft+=vec[i];}
+        return resright-resleft;
+    }
+};
